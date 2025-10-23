28da8401ccc987913b8f6ff120b472d1d5a57252
Niharika-Seth <niharikaseth1112@gmail.com>
2025-10-23 22:33:33 +0530

    Updated main urls.py by adding path to auth_apps

--- backend_todo/django_project/urls.py
+++ backend_todo/django_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("apis/v1/", include("apis.urls")),  # new (V1 stands for version 1)
+    path('auth/', include("auth_app.urls")),  
 ]
