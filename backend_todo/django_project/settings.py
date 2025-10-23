daa1d6eee41a299c5f44764ba55ff725dbe9f03d
Niharika-Seth <niharikaseth1112@gmail.com>
2025-10-23 21:07:01 +0530

    Added rest_framework and apis app in INSTALLED_APPS of settings.py

--- backend_todo/django_project/settings.py
+++ backend_todo/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos', 
+    'rest_framework',  # new
+    'apis',  # new
+    'todos',  # new
+
 ]
 
 MIDDLEWARE = [
