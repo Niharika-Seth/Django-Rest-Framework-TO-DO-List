38d912a2c1b8da4bd31ec1ae842bb533972587e8
Niharika-Seth <niharikaseth1112@gmail.com>
2025-10-24 23:02:06 +0530

    Updated views.py of todos app

--- backend_todo/todos/views.py
+++ backend_todo/todos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()  # needed for DRF router
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
